4554e952d41fbff0c1a88aed52b45728838489e7
Josh Rubenoff <1328849+jrubenoff@users.noreply.github.com>
2025-10-29 14:20:52 -0700

    Improve admin preview layouts

--- .eleventy.js
+++ .eleventy.js
@@ -58,6 +58,7 @@ export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./fonts/");
   eleventyConfig.addPassthroughCopy("./images/");
   eleventyConfig.addPassthroughCopy("./js/");
+  eleventyConfig.addPassthroughCopy("./admin/");
 
   eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
 
